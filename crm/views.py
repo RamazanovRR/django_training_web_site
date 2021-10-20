17a68c5b843a4893f4261cf24fb9badc9778095e
Roman Ramazanov <r.ramazanov@iteratia.com>
2021-10-20 17:48:47 +0700

    added submit form

--- crm/views.py
+++ crm/views.py
@@ -7,3 +7,13 @@ def first_page(request):
     return render(request, './index.html', {
         'object_list': object_list
     })
+
+def thanks_page(request):
+    name = request.POST['name']
+    phone = request.POST['phone']
+    element = Order(order_name = name, order_phone = phone)
+    element.save()
+    return render(request, './thanks_page.html', {
+        'name': name,
+        'phone': phone
+    })
