53f47c75481ccb7c1655698b82b6a801e1451bf9
Roman Ramazanov <r.ramazanov@iteratia.com>
2021-10-24 16:36:31 +0700

    create app "price"

--- crm/views.py
+++ crm/views.py
@@ -2,13 +2,24 @@ from django.shortcuts import render
 from .models import Order
 from .forms import OrderForm
 from cms.models import CmsSlider
+from price.models import PriceCard
+from price.models import PriceTable
 
 # Create your views here.
 def first_page(request):
     slider_list = CmsSlider.objects.all()
-    return render(request, './index.html', {
+    pc_1 = PriceCard.objects.get(pk=1)
+    pc_2 = PriceCard.objects.get(pk=2)
+    pc_3 = PriceCard.objects.get(pk=3)
+    pt_list = PriceTable.objects.all()
+    obj_dictionary = {
         'slider_list': slider_list,
-    })
+        'pc_1': pc_1,
+        'pc_2': pc_2,
+        'pc_3': pc_3,
+        'pt_list': pt_list
+    }
+    return render(request, './index.html', obj_dictionary)
 
 def thanks_page(request):
     name = request.POST['name']
